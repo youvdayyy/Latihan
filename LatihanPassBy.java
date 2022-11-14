3fc71250b01299822bae5b0e08fe5e787afbd487
youvdayyy <117829746+youvdayyy@users.noreply.github.com>
2022-11-14 18:56:02 +0700

    Add files via upload

---
+++ LatihanPassBy.java
@@ -0,0 +1,16 @@
+public class LatihanPassBy {
+
+	public static void main(String[] args) {
+		
+		//Pass by Value
+		int a = 1;
+		int duplikatA = a;
+		
+		duplikatA = 40;
+		
+		System.out.println(a);
+		System.out.println(duplikatA);
+		
+	}
+
+}
